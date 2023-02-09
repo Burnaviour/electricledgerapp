45fdd94364c2f372994468e4d826c12f34c10f34
Burnaviour <meharmuzafar760@gmail.com>
2023-02-10 00:36:04 +0500

    fixed userdashboard

--- src/new/UserDashboard.js
+++ src/new/UserDashboard.js
@@ -1,67 +1,3 @@
-<<<<<<< HEAD:src/components/UserDashboard.js
-import { useNavigate } from "react-router-dom";
-import { Button } from "react-bootstrap";
-
-export default function UserDashboard() {
-  const navigator = useNavigate();
-  function handleClick(e) {
-    if (e.target.id === "btn-1") {
-      navigator("/user-query-bill");
-    }
-    if (e.target.id === "btn-2") {
-      navigator("/generate-bill");
-    }
-    if (e.target.id === "btn-3") {
-      navigator("/user-query-history");
-    }
-  }
-  const prices = JSON.parse(localStorage.getItem("prices"));
-  const cnic = JSON.parse(localStorage.getItem("result"))[0].value.cnic;
-  return (
-    <>
-      <h1>User Dashboard</h1>
-      <h2>Welcome {localStorage.getItem("username")}</h2>
-      <h3>CNIC : {cnic}</h3>
-      <h3>Organization : {localStorage.getItem("orgName")}</h3>
-      <h3>prices </h3>
-      <h4>Service Chrages {prices.servicesCharges} PKR</h4>
-      <h4>Tax {prices.tax} %</h4>
-      <h4>Unit Price {prices.unitPrice} PKR</h4>
-      <div>
-        <Button
-          id="btn-1"
-          variant="success"
-          className="mx-4 my-4"
-          type="button"
-          onClick={handleClick}
-        >
-          Query Bill Details
-        </Button>
-
-        <Button
-          id="btn-2"
-          variant="success"
-          className="mx-4 my-4"
-          type="button"
-          onClick={handleClick}
-        >
-          Generate Bill
-        </Button>
-
-        <Button
-          id="btn-3"
-          variant="success"
-          className="mx-4 my-4"
-          type="button"
-          onClick={handleClick}
-        >
-          Bill History
-        </Button>
-      </div>
-    </>
-  );
-}
-=======
 import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
@@ -111,4 +47,3 @@ export default function UserDashboard() {
     </>
   );
 }
->>>>>>> origin/final:src/new/UserDashboard.js
